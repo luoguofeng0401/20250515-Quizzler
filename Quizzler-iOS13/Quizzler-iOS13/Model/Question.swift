49af7ff65de99023a229adb586225551849b995a
luoguofeng0401 <56261249+luoguofeng0401@users.noreply.github.com>
2024-05-15 05:04:58 +0800

    Add files via upload

---
+++ Quizzler-iOS13/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Guo-Feng Luo on 2024/5/15.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question{
+    let q: String
+    let a: String
+}
